5212d7bd00e1d2fa4731286a791a0e7ff6d6e9e1
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-03-08 02:26:32 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
-import userData from './userData.json';
-import friends from './friends.json';
-import transactions from './transactions.json';
+import userData from '../userData.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
 import Profile from './Profile/Profile';
 import FriendList from './FriendList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
